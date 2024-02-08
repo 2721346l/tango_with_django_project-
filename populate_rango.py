3ea51911affb5248bfa9d3b9f85a9b8a88b90c7d
“Jiayu <2721346L@student.gla.ac.uk>
2024-02-08 02:31:59 +0000

    chaper 5

--- populate_rango.py
+++ populate_rango.py
@@ -8,11 +8,11 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-        {'title': 'OfficialPython Tutorial',
+        {'title':'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/'},
         {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'LearnPythonin10 Minutes',
+        {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/'}
     ]
 
@@ -21,7 +21,7 @@ def populate():
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/'},
-        {'title':'HowtoTangowith Django',
+        {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/'}
     ]
 
@@ -32,13 +32,14 @@ def populate():
          'url':'http://flask.pocoo.org'}
     ]
 
-    cats ={'Python':{'pages':python_pages},
-           'Django':{'pages': django_pages},
-           'OtherFrameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
     
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -53,8 +54,10 @@ def add_page(cat, title,url, views=0):
     p.save()
     return p 
 
-def add_cat(name):
+def add_cat(name,views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c 
 
